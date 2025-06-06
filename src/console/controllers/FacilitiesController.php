b905d8c37582b25725d30b614b2081f0d8b43ce0
Kenneth den Hollander <kenneth@recranet.com>
2025-06-06 09:56:39 +0200

    Added console command

---
+++ src/console/controllers/FacilitiesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace recranet\craftrecranetbooking\console\controllers;
+
+use Craft;
+use craft\console\Controller;
+use recranet\craftrecranetbooking\models\Facility;
+use yii\console\ExitCode;
+use recranet\craftrecranetbooking\elements\Facility as FacilityElement;
+
+/**
+ * Facilities controller
+ */
+class FacilitiesController extends Controller
+{
+    public $defaultAction = 'index';
+
+    public function options($actionID): array
+    {
+        $options = parent::options($actionID);
+        switch ($actionID) {
+            case 'index':
+                // $options[] = '...';
+                break;
+        }
+        return $options;
+    }
+
+    /**
+     * _recranet-booking/facilities command
+     */
+    public function actionIndex(): int
+    {
+        $facility = new Facility();
+        $facility->title = 'Test Facility';
+        $facility->recranetBookingId = 12345;
+
+        $facility->validate();
+
+        $facilityElement = new FacilityElement();
+        $facilityElement->title = $facility->title;
+        $facilityElement->recranetBookingId = $facility->recranetBookingId;
+
+        dump(Craft::$app->getElements()->saveElement($facilityElement));
+
+
+        return ExitCode::OK;
+    }
+}
