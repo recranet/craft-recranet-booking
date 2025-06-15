76a44af601240cbc9f7d2d66830448375a532f65
Kenneth den Hollander <kenneth@recranet.com>
2025-06-16 00:13:24 +0200

    Added twig variable

--- src/variables/RecranetBookingVariable.php
+++ src/variables/RecranetBookingVariable.php
@@ -2,7 +2,15 @@
 
 namespace recranet\craftrecranetbooking\variables;
 
+use craft\helpers\App;
+use recranet\craftrecranetbooking\RecranetBooking;
+
 class RecranetBookingVariable
 {
+    public function getOrganizationId() : string
+    {
 
+        return App::parseEnv(RecranetBooking::getInstance()->getSettings()->organizationId);
+        ;
+    }
 }
